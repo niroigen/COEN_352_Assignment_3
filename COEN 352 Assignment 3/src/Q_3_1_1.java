a89c0a20f7796c96c99df2f3261e21c0493d2b45
N_SUNTH <N_SUNTH@BISHOP.ENCS.concordia.ca>
2018-10-30 21:28:46 -0400

    Adding Q_3_1_1

---
+++ COEN 352 Assignment 3/src/Q_3_1_1.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Q_3_1_1 {
+	HashMap<String, Double> gradesMap;
+	
+	Q_3_1_1() {
+		init();
+	}
+	
+	private void init() {
+		gradesMap = new HashMap<String, Double>();
+		
+		gradesMap.put("A+", 4.33);
+		gradesMap.put("A", 4.00);
+		gradesMap.put("A-", 3.67);
+		gradesMap.put("B+", 3.33);
+		gradesMap.put("B", 3.00);
+		gradesMap.put("B-", 2.67);
+		gradesMap.put("C+", 2.33);
+		gradesMap.put("C", 2.00);
+		gradesMap.put("C-", 1.67);
+		gradesMap.put("D", 1.00);
+		gradesMap.put("F", 0.00);
+	}
+	
+	public void calculateAverage(String gradesString) {
+		String[] grades = gradesString.split(" ");
+		
+		double average = 0.0;
+		int size = grades.length;
+		for (int i = 0; i < size; i++) {
+			average += gradesMap.get(grades[i]);
+		}
+		
+		average /= size;
+		average = Math.round(average * 100.0) / 100.0;
+		
+		System.out.println("Average grade is " + average);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Enter your grades");
+		Scanner scanner = new Scanner(System.in);
+		String grades = scanner.nextLine();
+		
+		Q_3_1_1 gradesTool = new Q_3_1_1();
+		
+		gradesTool.calculateAverage(grades);
+	}
+}
