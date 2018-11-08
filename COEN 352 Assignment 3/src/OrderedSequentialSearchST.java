e76e1db8f885d019f7dbf86c535c924e82177117
N_SUNTH <N_SUNTH@HEYMANS.ENCS.concordia.ca>
2018-11-07 19:58:42 -0500

    Adding nodes in sequential order

--- COEN 352 Assignment 3/src/OrderedSequentialSearchST.java
+++ COEN 352 Assignment 3/src/OrderedSequentialSearchST.java
@@ -14,58 +14,36 @@ public class OrderedSequentialSearchST<Key extends Comparable<Key>, Value> {
 		}
 	}
 	
-	public void deleteValue(Value val) {
-		Node previousNode = first;
-		
-		for (Node x = first; x != null; x = x.next) {
-			if (val.equals(x.val)) {
-				if (x.equals(first)) first = x.next;
-				else previousNode.next = x.next;
-				
-				return;
-			} 
-			
-			previousNode = x;
-		}
-		
-		System.out.println("No value " + val + " has been found in the linked list");
-	}
-	
 	public void put(Key key, Value val)
 	{ // Search for key. Update value if found; grow table if new.
+		if (first == null) {
+			first = new Node(key, val, first);
+			return;
+		}
+		
 		Node previousNode = first;
 		
 		for (Node x = first; x != null; x = x.next) {
 			if (x.key.compareTo(key) > 0) {
-				Key tempKey = key;
-				Value tempValue = val;
+				if (first.equals(previousNode)) first = new Node(key, val, x);
+				else previousNode.next = new Node(key, val, x);
 				
-				key = x.key;
-				val = x.val;
-				
-				x.key = tempKey;
-				x.val = tempValue;
+				return;
 			}
 			
 			previousNode = x;
 		}
 		
-		first = new Node(key, val, first); // Search miss: add new node.
+		previousNode.next = new Node(key, val, null);
+		 // Search miss: add new node.
+	}
+	
+	private void createNewFirstNode(Key key, Value val) {
+		first = new Node(key, val, first);
 	}
 	
 	public void printValues() {
 		for (Node x = first; x != null; x = x.next)
 			System.out.println(x.key + " " + x.val);
-	}
-	
-	public void searchForValue(Value val) {
-		for (Node x = first; x != null; x = x.next) {
-			if (val.equals(x.val)) {
-				System.out.println("Value found with key " + x.key);
-				return;
-			}
-		}
-		
-		System.out.println("Value " + val.toString() + " not found");
 	}
 }
--- COEN 352 Assignment 3/src/Q_3_1_3.java
+++ COEN 352 Assignment 3/src/Q_3_1_3.java
@@ -3,13 +3,11 @@ public class Q_3_1_3 {
 	public static void main(String[] args) {
 		OrderedSequentialSearchST list = new OrderedSequentialSearchST();
 		
-		list.put('A', 1);
-		list.put('D', 2);
-		list.put('B', 3);
-		list.put('C', 4);
-		list.printValues();
-		list.searchForValue(-1);
-		list.deleteValue(1);
+		list.put('S', 10);
+		list.put('B', 1);
+		list.put('A', 2);
+		list.put('C', 3);
+		list.put('D', 9);
 		list.printValues();
 	}
 }
