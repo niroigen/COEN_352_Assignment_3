9354bd7cb7fe3b822ef32ed7ac26826e1992dc7c
Niroigen Suntharam <niroigen@gmail.com>
2018-11-09 22:53:44 -0500

    Prompting user for the text

--- COEN 352 Assignment 3/src/Q_3_4_4.java
+++ COEN 352 Assignment 3/src/Q_3_4_4.java
@@ -1,9 +1,14 @@
 import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Q_3_4_4 {
 	public static void main(String[] args) {
-        findAAndM("SEARCHXMPL");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the keys separated by spaces:");
+		String keys = scanner.nextLine().replaceAll(" ", "");
+		
+        findAAndM(keys);
     }
 	
 	private static void findAAndM(String text) {
@@ -14,23 +19,16 @@ public class Q_3_4_4 {
 					if (!indices.add((a * calculateK(text.charAt(i))) % M)) break;
 				}
 				
-				System.out.println("M = " + M);
-				System.out.println("a = " + a);
-				
 				if (text.length() == indices.size()) {
-					System.out.println("A perfect hash function");
+					System.out.println("M = " + M);
+					System.out.println("a = " + a);
 					return;
-				} else {
-					System.out.println("A non perfect hash function where the indices size are " + indices.size());
 				}
 			}
 		}
 	}
 	
 	private static int calculateK(char character) {
-		System.out.println((int)character);
-		System.out.println((int)'A');
-		System.out.println((int)character - (int)'A');
 		return (int)character - (int)'A';
 	}
 }
